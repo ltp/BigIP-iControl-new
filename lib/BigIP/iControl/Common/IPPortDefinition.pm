dd6d06536697360001e9464d20fa85ed20f194af
Luke Poskitt <luke.poskitt@gmail.com>
2013-06-10 16:59:35 +1000

    Added BigIP::iControl::IPPortDefinition class

---
+++ lib/BigIP/iControl/Common/IPPortDefinition.pm
@@ -0,0 +1,11 @@
+package BigIP::iControl::Common::IPPortDefinition;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+sub new {
+	my( $class, $icontrol, %args ) = @_;
+	my $self = bless {}, $class;
+}
