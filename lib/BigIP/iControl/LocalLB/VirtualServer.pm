9c46b9ef701a4d2c756481612046085a58dd05ff
Luke Poskitt <luke.poskitt@gmail.com>
2013-07-05 16:48:08 +1000

    Added rules() method

--- lib/BigIP/iControl/LocalLB/VirtualServer.pm
+++ lib/BigIP/iControl/LocalLB/VirtualServer.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use Scalar::Util qw(weaken);
 use BigIP::iControl::Common::IPPortDefinition;
+use BigIP::iControl::LocalLB::VirtualServerRule;
 
 our $VERSION = '0.01';
 
@@ -27,6 +28,19 @@ sub destination {
 						data 		=> { virtual_servers => [ $self->{name} ] }
 					)
 		}[0] )
+}
+
+sub rules { 
+	my $self = shift;
+	$self->{name} or return;
+	my @rules = map { BigIP::iControl::LocalLB::VirtualServerRule->new( $self->{_icontrol}, $_ ) }
+		@{ @{$self->{_icontrol}->_request(module		=> 'LocalLB', 
+						interface 	=> 'VirtualServer',
+						method 		=> 'get_rule',
+						data 		=> { virtual_servers => [ $self->{name} ] }
+					)
+		}[0] };
+	return @rules
 }
 
 sub state { 
