52dec2328a3938d0f1aee64a4821484563cd0977
Luke Poskitt <luke.poskitt@gmail.com>
2013-07-08 09:54:30 +1000

    Added BigIP::iControl::LocalLB::Rule class

---
+++ lib/BigIP/iControl/LocalLB/Rule.pm
@@ -0,0 +1,38 @@
+package BigIP::iControl::LocalLB::Rule;
+
+use strict;
+use warnings;
+
+use Scalar::Util qw(weaken);
+
+sub new {
+	my( $class, $icontrol, $name, %args ) = @_;
+	my $self = bless {}, $class;
+	$self->{ name } = $name;
+	weaken( $self->{_icontrol} = $icontrol );
+	return $self;
+}
+
+sub name {
+	my $self = shift;
+	$self->{name} or return;
+	return @{ $self->{_icontrol}->_request(module	=> 'LocalLB',
+					      interface	=> 'Rule',
+					      method	=> 'query_rule',
+					      data	=> { rule_names => [ $self->{name} ] }
+					)
+		}[0]->{rule_name};
+}
+
+sub definition {
+	my $self = shift;
+	$self->{name} or return;
+	return @{ $self->{_icontrol}->_request(module	=> 'LocalLB',
+					      interface	=> 'Rule',
+					      method	=> 'query_rule',
+					      data	=> { rule_names => [ $self->{name} ] }
+					)
+		}[0]->{rule_definition};
+}
+
+1;
