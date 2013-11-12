3869e732777b41857727bb66eb60e4e409b555e0
Luke Poskitt <luke.poskitt@gmail.com>
2013-11-12 14:23:56 +1100

    Added POD

--- lib/BigIP/iControl/LocalLB/Class/StringClass.pm
+++ lib/BigIP/iControl/LocalLB/Class/StringClass.pm
@@ -46,3 +46,79 @@ sub member {
 
 
 1;
+
+=head1 NAME
+
+BigIP::iControl::LocalLB::Class::StringClass - Class for operations with LocalLB string classes.
+
+=head1 DESCRIPTION
+
+This module provides an interface to the LocalLB (LTM) String Class methods.
+
+=head1 METHODS
+
+=head3 member( $SCALAR )
+
+Returns the specified string class member of the target class identified by the 
+named key parameter as a L<BigIP::iControl::LocalLB::Class::Member> object;
+
+=head3 members
+
+Returns an array of scalars representing the key values of all members in the
+target string class.
+
+=head1 AUTHOR
+
+Luke Poskitt, C<< <ltp at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-bigip-icontrol at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=BigIP-iControl-LocalLB-Class-StringClass>.  
+I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc BigIP::iControl::LocalLB::Class::StringClass
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=BigIP-iControl-LocalLB-Class-StringClass>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/BigIP-iControl-LocalLB-Class-StringClass>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/BigIP-iControl-LocalLB-Class-StringClass>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/BigIP-iControl-LocalLB-Class-StringClass/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Luke Poskitt.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
