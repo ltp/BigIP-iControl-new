e6c76157edf644acbd22d2983dc3ce73503e45e7
Luke Poskitt <luke.poskitt@gmail.com>
2013-11-27 15:48:34 +1100

    Class now works correctly. Auto-generated accessor method rather than static.

--- lib/BigIP/iControl/LocalLB/VirtualServer/VirtualServerPersistence.pm
+++ lib/BigIP/iControl/LocalLB/VirtualServer/VirtualServerPersistence.pm
@@ -3,21 +3,23 @@ package BigIP::iControl::LocalLB::VirtualServer::VirtualServerPersistence;
 use strict;
 use warnings;
 
+our $VERSION    = '0.01';
+our @ACCESSORS  = qw(profile_name default_profile);
+
+{ no strict 'refs';
+        foreach my $attr ( @ACCESSORS ) { 
+                *{ __PACKAGE__ ."::$attr" } = sub { return $_[0]->{ $attr } } 
+        }   
+}
+
 sub new {
 	my( $class, $args ) = @_;
 	my $self = bless {}, $class;
-	$self->{profile_name} = $args->{profile_name};
-	$self->{default_profile} = $args->{default_profile};
+	$self->{profile_name} = $args->[0]->{profile_name};
+	$self->{default_profile} = $args->[0]->{default_profile};
 	return $self;
 }
 
-sub profile_name {
-	return $_[0]->{profile_name}
-}
-
-sub default_profile {
-	return $_[0]->{default_profile}
-}
 
 1;
 
