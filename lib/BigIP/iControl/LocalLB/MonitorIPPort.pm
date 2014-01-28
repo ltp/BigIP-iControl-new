d2be19fdb9ccc9bbab96da2728faa2d013132a4d
Luke Poskitt <luke.poskitt@gmail.com>
2014-01-28 17:09:53 +1100

    Added BigIP::iControl::LocalLB::MonitorIPPort module

---
+++ lib/BigIP/iControl/LocalLB/MonitorIPPort.pm
@@ -0,0 +1,20 @@
+package BigIP::iControl::LocalLB::MonitorIPPort;
+
+use strict;
+use warnings;
+
+use BigIP::iControl::Common::IPPortDefinition;
+
+our $VERSION = '0.01';
+
+sub new {
+	my( $class, $args ) = @_;
+	my $self = bless {}, $class;
+	use Data::Dumper; print Dumper( $args );
+	$self->{ipport} = BigIP::iControl::Common::IPPortDefinition->new( $args->{enabled_state} );
+	$self->{address_type} = $args->{address_type};
+	$self->{instance} = $args->{instance};
+	return $self
+}
+
+1;
